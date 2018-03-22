0f8d3e36113f82933ce41f97ccaf23333e44aa55
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2018-03-22 09:10:56 -0400

    ssss

---
+++ helloWorld/src/helloWorld/HelloWorld.java
@@ -0,0 +1,37 @@
+package helloWorld;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class HelloWorld extends Application
+{
+	public static void main(String[] args)
+	{
+        launch(args);
+    }
+
+	public void start (Stage primaryStage)
+	{
+		primaryStage.setTitle("Does This Button Work?");
+		Button btn = new Button();
+		btn.setText("Does This Button Work?");
+		btn.setOnAction(new EventHandler<ActionEvent>()
+		{
+			public void handle (ActionEvent event)
+			{
+				System.out.println("No");
+				
+			}
+		});
+	StackPane root = new StackPane();
+	root.getChildren().add(btn);
+	primaryStage.setScene(new Scene(root, 300, 250));
+	primaryStage.show();
+	}
+}
